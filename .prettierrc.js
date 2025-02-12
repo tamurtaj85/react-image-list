8e7a7f160cf030084169e22414285dc72219e194
Tamur Ahmed <tamurahmed@Tamurs-MacBook-Air.local>
2025-02-12 22:01:44 +0500

    init commit

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "es5",
+  singleQuote: true,
+  jsxSingleQuote: true,
+};
+
+export default config;
---
+++ src/components/imageListContainer.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const ImageListContainer = () => {
+  return <div>ImageListContainer</div>;
+};
